141cf0ce6d1cd0fed8ee10dc461f75e77d89d1ad
shubhamkokul <shubhamkokul@gmail.com>
2019-06-14 16:22:42 -0400

    Office Commit

--- src/create_csv.py
+++ src/create_csv.py
@@ -5,7 +5,6 @@ import re
 from urllib.request import Request, urlopen
 
 
-
 def scrape_poets_links(url):
     global poet_link
     poet_link = []
@@ -14,9 +13,9 @@ def scrape_poets_links(url):
     req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
     plain = urlopen(req).read()
     soup = BeautifulSoup(plain, "html.parser")
-    mydivs = soup.find_all("div",{"class":"poet"})
+    mydivs = soup.find_all("div", {"class": "poet"})
     for s in mydivs:
-        if(s.find('a')['href'] not in poet_link):
+        if (s.find('a')['href'] not in poet_link):
             poet_link.append(s.find('a')['href'])
     for s1 in poet_link:
         match = re.search(r'https://ganjoor.net/(\w+)', s1)
@@ -26,9 +25,8 @@ def scrape_poets_links(url):
     return poet_name
 
 
-
-def scrape_subsection_links(url,poet_name_value):
-    s1= []
+def scrape_subsection_links(url, poet_name_value):
+    s1 = []
     final_sub_links = []
     final_sub_section_name = []
     req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
@@ -39,15 +37,15 @@ def scrape_subsection_links(url,poet_name_value):
     create_link = 'https://ganjoor.net/{}/'.format(poet_name_value)
     for s in mydivs:
         temp = r.findall(str(s.find('a')))
-        if(temp != []):
+        if (temp != []):
             s1.append(temp)
     for sublinks in range(len(s1)):
-        if(s1[sublinks][0] not in final_sub_links):
-            z = re.match(create_link+'(.*)',str(s1[sublinks][0]))
-            if(z):
+        if (s1[sublinks][0] not in final_sub_links):
+            z = re.match(create_link + '(.*)', str(s1[sublinks][0]))
+            if (z):
                 final_sub_links.append(s1[sublinks][0])
     for value in final_sub_links:
-        match = re.search(r''+create_link+'(.*)', value)
+        match = re.search(r'' + create_link + '(.*)', value)
         if match:
             final_sub_section_name.append(match.group(1))
     return final_sub_links, final_sub_section_name
@@ -61,15 +59,15 @@ def scrape_poem_count(url):
     count = 0
     for s in mydivs:
         match = re.search(r'.*sh\d+', str(s))
-        if(match):
-            count = count+1
+        if (match):
+            count = count + 1
     return count
 
 
 def final_call():
     scrape_poets_links('https://ganjoor.net/');  # Perfect
     for s in range(len(poet_link)):
-        f = open("/home/castiel/PycharmProjects/Poems/data/input/{}.txt".format(poet_name[s]), "a")
+        f = open("/home/5pk/PycharmProjects/Poems/data/input/{}.txt".format(poet_name[s]), "a")
         f.write('poem_link,arabic_poet,subsection,poems')
         f.write("\n")
         final_sub_links, final_sub_section_name = scrape_subsection_links(poet_link[s], poet_name[s])
@@ -81,20 +79,15 @@ def final_call():
             f.write("\n")
         f.close()
 
+
 def create_name_list():
     poet_name = scrape_poets_links('https://ganjoor.net/')
-    f = open("/home/castiel/PycharmProjects/Poems/data/input/names.txt", "a")
+    f = open("/home/5pk/PycharmProjects/Poems/data/input/names.txt", "a")
     for s in poet_name:
         f.write('Name')
         f.write(s)
         f.write("\n")
 
 
-
-
 final_call()
 create_name_list()
-
-
-
-
--- src/scrape_poem.py
+++ src/scrape_poem.py
@@ -1,19 +1,18 @@
 from bs4 import BeautifulSoup
 import re
 from urllib.request import Request, urlopen
-from pandas import DataFrame, Series
 import pandas as pd
 import os
 
 
 def read_csv(input_user):
-    links = [];
-    sublimits = [];
-    poems_count = [];
-    input_data = pd.read_csv("/home/castiel/PycharmProjects/Poems/data/input/{}.txt".format(input_user))
+    links = []
+    sublimits = []
+    poems_count = []
+    input_data = pd.read_csv("/home/5pk/PycharmProjects/Poems/data/input/{}.txt".format(input_user))
     poet = input_data.iloc[0].iloc[1]
     print('Poet {}'.format(poet.strip()))
-    os.mkdir('/home/castiel/PycharmProjects/Poems/data/{}/'.format(poet.strip()))
+    os.mkdir('/home/5pk/PycharmProjects/Poems/data/{}/'.format(poet.strip()))
     for s in input_data['poem_link']:
         links.append(s)
     for s in input_data['subsection']:
@@ -22,12 +21,12 @@ def read_csv(input_user):
         poems_count.append(s)
 
     for a in range(len(links)):
-        os.mkdir('/home/castiel/PycharmProjects/Poems/data/{}/{}'.format(poet.strip(),sublimits[a]))
-        if(poems_count[a]>0):
-            for s in range(1, poems_count[a]+1, 1):
+        os.mkdir('/home/5pk/PycharmProjects/Poems/data/{}/{}'.format(poet.strip(), sublimits[a]))
+        if (poems_count[a] > 0):
+            for s in range(1, poems_count[a] + 1, 1):
                 s1 = 'sh{}'.format(s)
-                scrape_poems('{}/{}/'.format(links[a],str(s1)))
-                clear_poems(poet.strip(),sublimits[a],s1)
+                scrape_poems('{}/{}/'.format(links[a], str(s1)))
+                clear_poems(poet.strip(), sublimits[a], s1)
         else:
             scrape_poems('{}/{}/'.format(links[a], str(sublimits[a])))
             clear_poems(poet.strip(), sublimits[a], sublimits[a])
@@ -38,11 +37,11 @@ def scrape_poems(url):
     plain = urlopen(req).read()
     soup = BeautifulSoup(plain, "html.parser")
     global mydivs
-    mydivs=[]
-    mydivs = soup.find_all("div",{"class":"m1"})
+    mydivs = []
+    mydivs = soup.find_all("div", {"class": "m1"})
     global mydivs2
     mydivs2 = []
-    mydivs2 = soup.find_all("div",{"class":"m2"})
+    mydivs2 = soup.find_all("div", {"class": "m2"})
 
 
 def clear_poems(data1, data2, data3):
@@ -50,34 +49,31 @@ def clear_poems(data1, data2, data3):
     second_stance = []
     for s in mydivs:
         p = re.compile(r'<.*?>')
-        first_stance.append(p.sub(" ",str(s)))
+        first_stance.append(p.sub(" ", str(s)))
     for s in mydivs2:
         p = re.compile(r'<.*?>')
-        second_stance.append(p.sub(" ",str(s)))
-    length = len(first_stance) if len(first_stance)>len(second_stance) else len(second_stance)
+        second_stance.append(p.sub(" ", str(s)))
+    length = len(first_stance) if len(first_stance) > len(second_stance) else len(second_stance)
     first_stance_length = len(first_stance)
     second_stance_length = len(second_stance)
-    c = 0;
-    f = open("/home/castiel/PycharmProjects/Poems/data/{}/{}/{}.txt".format(data1,data2,data3), "a")
-    for i in range(length*2):
-        if i%2 == 0:
+    c = 0
+    f = open("/home/5pk/PycharmProjects/Poems/data/{}/{}/{}.txt".format(data1, data2, data3), "a")
+    for i in range(length * 2):
+        if i % 2 == 0:
             f.write(first_stance[c])
             f.write("\n")
         else:
-            if(second_stance_length>c):
+            if (second_stance_length > c):
                 f.write(second_stance[c])
             f.write("\n")
-            c=c+1;
+            c = c + 1
     f.close()
 
 
 def final_call():
-    input_data = pd.read_csv("/home/castiel/PycharmProjects/Poems/data/names.txt")
+    input_data = pd.read_csv("/home/5pk/PycharmProjects/Poems/data/names.txt")
     for s in input_data['Names']:
         read_csv(s)
 
 
-
 final_call()
-
-
